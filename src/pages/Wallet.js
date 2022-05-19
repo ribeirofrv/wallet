919ffa4708440bf9fe4ae5df66bab0edc9c04d96
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-19 18:43:10 -0300

    Iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
     return <div>TrybeWallet</div>;
   }
