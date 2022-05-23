1ceec962bba2323fcba3a50c2bc8dce5127d92d0
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-22 22:33:40 -0300

    fix: Formato do State `user` agora com apenas uma chave 👀

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,15 +1,13 @@
 import { ADD_EMAIL } from '../actions';
 
-const INITIAL_STATE = { user: { email: '' } };
+const INITIAL_STATE = { email: '' };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case ADD_EMAIL:
     return {
       ...state,
-      user: {
-        email: action.payload,
-      },
+      email: action.payload,
     };
   default:
     return state;
