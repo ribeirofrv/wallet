55da5dc2a653cde1bc91479d0ba6d52014bc10ce
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-23 19:02:07 -0300

    feat: Adiciona action thunk para buscar code das moedas na API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_CURRENCIES = 'ADD_CURRENCIES';
 
-export function addEmailAction(email) {
-  return {
-    type: ADD_EMAIL,
-    payload: email,
-  };
-}
+export const addEmailAction = (email) => ({ type: ADD_EMAIL, payload: email });
+
+const addCurrencyAction = (currencies) => ({
+  type: ADD_CURRENCIES,
+  payload: currencies,
+});
+
+export const getCurrency = () => async (dispatch) => {
+  const apiData = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const currencies = await apiData.json();
+
+  delete currencies.USDT;
+  const currenciesKeys = Object.keys(currencies);
+
+  dispatch(addCurrencyAction(currenciesKeys));
+};
