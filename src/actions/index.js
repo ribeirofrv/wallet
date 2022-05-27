4548e895f9ba2cd7d6f62e07c5fa51e68be24475
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-27 15:39:45 -0300

    feaCria action para adicionar despesa ao store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
 export const ADD_EMAIL = 'ADD_EMAIL';
 export const ADD_CURRENCIES = 'ADD_CURRENCIES';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
 
 export const addEmailAction = (email) => ({ type: ADD_EMAIL, payload: email });
 
 const addCurrencyAction = (currencies) => ({
   type: ADD_CURRENCIES,
   payload: currencies,
+});
+
+const addExpenseAction = (expenses) => ({
+  type: ADD_EXPENSE,
+  payload: expenses,
 });
 
 export const getCurrency = () => async (dispatch) => {
@@ -16,4 +22,14 @@ export const getCurrency = () => async (dispatch) => {
   const currenciesKeys = Object.keys(currencies);
 
   dispatch(addCurrencyAction(currenciesKeys));
+};
+
+export const getExpense = (localState) => async (dispatch) => {
+  const apiData = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const currencies = await apiData.json();
+
+  delete currencies.USDT;
+  const expense = { localState, currencies };
+
+  dispatch(addExpenseAction(expense));
 };
