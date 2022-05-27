bf1454643e8340cb7ec3cce295c1778b9cfe2ec2
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-27 19:01:15 -0300

    feat: Implementa função que converte moedas em reais, soma gastos e exibe no header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,9 +5,25 @@ import { getCurrency } from '../actions';
 import ExpenseForm from '../components/ExpenseForm';
 
 class Wallet extends Component {
+  constructor() {
+    super();
+    this.getTotalExpense = this.getTotalExpense.bind(this);
+  }
+
   componentDidMount() {
     const { addCurrency } = this.props;
     addCurrency();
+  }
+
+  getTotalExpense() {
+    const { expense } = this.props;
+    const totalExpense = expense.reduce((total, expenses) => {
+      const { value, currency } = expenses;
+      const conversionRate = expenses.exchangeRates[currency].ask;
+      return total + (value * conversionRate);
+    }, 0);
+
+    return totalExpense.toFixed(2);
   }
 
   render() {
@@ -16,7 +32,7 @@ class Wallet extends Component {
       <>
         <header>
           <p data-testid="email-field">{email}</p>
-          <p data-testid="total-field">0</p>
+          <p data-testid="total-field">{this.getTotalExpense()}</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
         <main>
@@ -29,6 +45,7 @@ class Wallet extends Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expense: state.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
