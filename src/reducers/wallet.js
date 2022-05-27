e12ac8a2458937df1df97a8f9aa1dba2d573fee2
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-27 16:05:22 -0300

    feat: Cria case para adicionar despesa

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { ADD_CURRENCIES } from '../actions';
+import { ADD_CURRENCIES, ADD_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -11,6 +11,17 @@ const wallet = (state = INITIAL_STATE, action) => {
     return {
       ...state,
       currencies: action.payload,
+    };
+  case ADD_EXPENSE:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        {
+          ...action.payload.localState,
+          exchangeRates: action.payload.currencies,
+        },
+      ],
     };
   default:
     return state;
